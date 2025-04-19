c3af1980d415c4d8a0195935383045317ccfb213
kalpesh95 <kalpeshlad95@gmail.com>
2025-04-19 22:34:55 +0530

    should block console

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-console.log('Hello');
-app.listen(process.env.PORT, () => {
-  console.log('Listen to server port 3000');
-});
+app.listen(process.env.PORT, () => {});
