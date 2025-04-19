e97a3013545d5da0ce6083883e36c6f22e652205
kalpesh95 <kalpeshlad95@gmail.com>
2025-04-19 22:02:34 +0530

    test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 require("dotenv").config();
 console.log(process.env.PORT);
 const app = express();
+console.log("bad log")
+
 app.listen(process.env.PORT, () => {
   console.log("Listen to server port 3000");
 })
