1a94048f6f7de40d57a0f9d0996e963e7df1264b
kalpesh95 <kalpeshlad95@gmail.com>
2025-04-19 22:04:03 +0530

    test bad commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 console.log(process.env.PORT);
 const app = express();
 console.log("bad log")
+console.log("bad log")
 
 app.listen(process.env.PORT, () => {
   console.log("Listen to server port 3000");
