ccc663cadd30fbaa0b6fc8c3a629223a570c6578
kalpesh95 <kalpeshlad95@gmail.com>
2025-04-19 22:08:28 +0530

    test hook

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("dotenv").config();
 console.log(process.env.PORT);
 const app = express();
-console.log("Hello")
 app.listen(process.env.PORT, () => {
   console.log("Listen to server port 3000");
 })
